eb002ea300a98dfe3db6bacf11e462838341a30b
caglar1996 <caglar.karabacak@outlook.com>
2022-12-12 09:45:16 +0300

    dev

---
+++ src/EFCoreTutorial.Data/Context/ApplicationDbContextDesingTimeFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace EFCoreTutorial.Data.Context
+{
+    public class ApplicationDbContextDesingTimeFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var context = new ApplicationDbContext();
+
+            
+
+            return context;
+        }
+    }
+}
---
+++ src/EFCoreTutorial.Data/Migrations/20221206122132_lentghChangeMig.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreTutorial.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class lentghChangeMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                schema: "dbo",
+                table: "students",
+                type: "nvarchar(350)",
+                maxLength: 350,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "first_name",
+                schema: "dbo",
+                table: "students",
+                type: "nvarchar(350)",
+                maxLength: 350,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                schema: "dbo",
+                table: "students",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(350)",
+                oldMaxLength: 350);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "first_name",
+                schema: "dbo",
+                table: "students",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(350)",
+                oldMaxLength: 350);
+        }
+    }
+}
